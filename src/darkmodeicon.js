fa91f255905f80c010f097d66dea3e9e7aff89b2
Chris Holland <zrenfire@gmail.com>
2023-07-20 14:36:43 -0400

    Chrome doesn't like returning outside a function

--- src/darkmodeicon.js
+++ src/darkmodeicon.js
@@ -1,8 +1,5 @@
 var isFirefox = typeof browser !== 'undefined'
 var isChrome = typeof browser === 'undefined'
-if (isFirefox) {
-	return
-}
 
 // https://stackoverflow.com/questions/58880234/toggle-chrome-extension-icon-based-on-light-or-dark-mode-browser
 // https://stackoverflow.com/questions/56393880/how-do-i-detect-dark-mode-using-javascript
@@ -12,27 +9,28 @@ if (isFirefox) {
 // Notes:
 // * addEventListener('change') doesn't seem to fire event in Chrome background script?!
 // * So it only updates the icon when extension is loaded. So restart Chrome after changing OS theme.
-
-const prefersDarkQuery = window.matchMedia("(prefers-color-scheme: dark)")
-function onThemeChange(e) {
-	console.log('prefersDarkQuery.change', prefersDarkQuery)
-	fetch('manifest.json').then((res) => res.json()).then(function(manifest) {
-		// console.log('manifest', manifest)
-		const theme_icons = manifest.browser_action.theme_icons
-		// console.log('theme_icons', theme_icons)
-		const iconThemeKey = prefersDarkQuery.matches ? 'light' : 'dark'
-		// console.log('iconThemeKey', iconThemeKey)
-		let iconPaths = {}
-		for (const icon of theme_icons) {
-			// console.log('icon', icon)
-			const sizeStr = '' + icon.size
-			iconPaths[sizeStr] = icon[iconThemeKey]
-		}
-		console.log('iconPaths', iconPaths)
-		chrome.browserAction.setIcon({
-			path: iconPaths,
+if (isChrome) {
+	const prefersDarkQuery = window.matchMedia("(prefers-color-scheme: dark)")
+	function onThemeChange(e) {
+		console.log('prefersDarkQuery.change', prefersDarkQuery)
+		fetch('manifest.json').then((res) => res.json()).then(function(manifest) {
+			// console.log('manifest', manifest)
+			const theme_icons = manifest.browser_action.theme_icons
+			// console.log('theme_icons', theme_icons)
+			const iconThemeKey = prefersDarkQuery.matches ? 'light' : 'dark'
+			// console.log('iconThemeKey', iconThemeKey)
+			let iconPaths = {}
+			for (const icon of theme_icons) {
+				// console.log('icon', icon)
+				const sizeStr = '' + icon.size
+				iconPaths[sizeStr] = icon[iconThemeKey]
+			}
+			console.log('iconPaths', iconPaths)
+			chrome.browserAction.setIcon({
+				path: iconPaths,
+			})
 		})
-	})
+	}
+	prefersDarkQuery.addEventListener('change', onThemeChange)
+	onThemeChange()
 }
-prefersDarkQuery.addEventListener('change', onThemeChange)
-onThemeChange()

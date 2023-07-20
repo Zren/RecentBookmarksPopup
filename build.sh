534817f3b829a3fb4c4694573326e0baf63ec42c
Chris Holland <zrenfire@gmail.com>
2023-07-20 11:25:00 -0400

    Add build scripts

---
+++ build.sh
@@ -0,0 +1,45 @@
+#/usr/bin/env sh
+
+projectName="RecentBookmarksPopup"
+
+### Firefox
+echo "[Firefox]"
+
+# Modify
+python3 ./preparebuild_firefox.py
+
+# Zip
+zipFilename="${projectName}-firefox.xpi"
+if [ -f "$zipFilename" ]; then
+	rm "$zipFilename"
+fi
+(cd ./src && zip \
+	-x "icons/rating*.png" \
+	-x "icons/rating*.svg" \
+	-r "./../${zipFilename}" \
+	./* \
+)
+
+
+### Chrome
+echo ""
+echo "[Chrome]"
+
+# Modify
+python3 ./preparebuild_chrome.py
+
+# Zip
+zipFilename="${projectName}-chrome.crx"
+if [ -f "$zipFilename" ]; then
+	rm "$zipFilename"
+fi
+(cd ./src && zip \
+	-x "faviconcacher.js" \
+	-x "icons/rating*.png" \
+	-x "icons/rating*.svg" \
+	-r "./../${zipFilename}" \
+	./* \
+)
+
+git checkout src/manifest.json
+

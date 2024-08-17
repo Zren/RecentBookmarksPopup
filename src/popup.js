bd3eba150cbda7c2dcee005e7b43c65300d659fa
Chris Holland <zrenfire@gmail.com>
2024-08-17 15:16:12 -0400

    Show elided bookmark title in tooltip

--- src/popup.js
+++ src/popup.js
@@ -301,6 +301,7 @@ var renderBookmarksList = function() {
 			['.bookmarks-item', 'attributes.href', bookmarkTreeNode.url],
 			['.bookmarks-item', 'attributes.aria-label', bookmarkTreeNode.title],
 			['.website-title', 'textContent', bookmarkTreeNode.title],
+			['.website-title', 'attributes.title', bookmarkTreeNode.title],
 			['.bookmark-tag', 'textContent', tag],
 			['.bookmark-tag', 'attributes.aria-label', tag],
 			['.bookmark-tag', 'attributes.style', tagStyle],
